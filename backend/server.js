49a5d472011b02f63bde363f7e8ee8995ef391c5
Luke Coty <Lukecoty4@gmail.com>
2026-08-20 18:37:26 -0400

    prepared for the deployment

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path";
 
 import gamesRoute from "./routes/gamesRoute.js";
 
 dotenv.config();
 
+const __dirname =path.resolve()
+
 const app = express();
 
 // middleware for parsing json body
 app.use(express.json());
-// middlware for handling cors policy
-app.use(cors());
+
+if(process.env.NODE_ENV !== "production") {
+    // middlware for handling cors policy
+    app.use(cors());
+}
 
 app.get("/", (req, res) => {
     res.send("Hello world!");
@@ -20,7 +26,13 @@ app.get("/", (req, res) => {
 
 app.use("/games", gamesRoute);
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
+    app.get("/{*splat}", (req, res) => {
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+})
+}
 
 mongoose
     .connect(process.env.MONGODBURL)
