cfd677fb12f29b03df95b7d9a378e9b347a241a3
Luke Coty <Lukecoty4@gmail.com>
2026-09-01 18:19:09 -0400

    Add platform color headers

--- frontend/src/components/VideogameCard.jsx
+++ frontend/src/components/VideogameCard.jsx
@@ -2,8 +2,10 @@ import { Link } from "react-router-dom";
 
 function VideoGameCard({ videogame }) {
     return(<div className="game-card">
+                <div className={`${videogame.console.toLowerCase().replace(/\s+/g, '-')}`}>
                 <h3 >{videogame.title}</h3>
                 <p>{videogame.console}</p>
+                </div>
                 <p>Played: {videogame.played ? "Yes" : "No"}</p>
                 <p>{`Rating: ${videogame.rating}/10`}</p>
                 <div>
